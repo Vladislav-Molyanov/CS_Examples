a50813796629f75efbdf4987b4b56148668f037e
Molyanov Vladislav <molyanov1488@gmail.com>
2022-08-29 01:48:50 +0400

    add Example005_Condition ifElse

---
+++ Example005_Condotion_ifElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лена" ) 
+{
+    Console.WriteLine("Ура, это же ЛЕНА");
+}
+else
+{
+    Console.Write ("Привет,");
+    Console.WriteLine(username);
+
+}
