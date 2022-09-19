88043b86c61541f7610cf088d5cfda4130d41657
Molyanov Vladislav <molyanov1488@gmail.com>
2022-09-19 18:11:18 +0400

    solved the problem of finding the sum of elements in odd positions.

---
+++ Example007_SierpinskiTriangle/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
+
+int xa = 1, ya = 1,
+    xb = 1, yb = 60,
+    xc = 50, yc = 30;
+ 
+Console.SetCursorPosition(xa, ya);
---
+++ Example008_Hoba/Program.cs
@@ -0,0 +1,29 @@
+﻿int GetPointArea(int x, int y ) 
+{
+    if (x > 0 && y > 0) {
+        return 1;
+    }
+
+    if (x < 0 && y > 0) {
+        return 2;
+    }
+
+    if (x < 0 && y < 0) {
+        return 3;
+    }
+
+    if (x > 0 && y < 0) {
+        return 4;
+    }
+
+    return 0;
+}
+
+Console.WriteLine("Input X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int areaNum = GetPointArea(x, y);
+Console.WriteLine($"Area = {areaNum}");
